96d5366771d51df48c6370e50830200c53da201e
KanBen86 <benjamin.kanzler@gmail.com>
2019-03-21 10:02:43 +0100

    Id-Klasse für den Zusammengesetzten Schlüssel der Bankverbindung erstellt

--- src/main/java/de/dhbw/skatula/jpa/BankverbindungId.java
+++ src/main/java/de/dhbw/skatula/jpa/BankverbindungId.java
@@ -7,27 +7,17 @@ package de.dhbw.skatula.jpa;
 
 import java.io.Serializable;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
 
 /**
  *
  * @author Benjamin Kanzler
  */
-@Entity
-@IdClass(BankverbindungId.class)
-public class Bankverbindung implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    @Id
+public class BankverbindungId implements Serializable {
+    
     private String bic = "";
 
-    @Id
     private String iban = "";
-
-    private String insitut;
 
     public String getBic() {
         return bic;
@@ -45,20 +35,11 @@ public class Bankverbindung implements Serializable {
         this.iban = iban;
     }
 
-    public String getInsitut() {
-        return insitut;
-    }
-
-    public void setInsitut(String insitut) {
-        this.insitut = insitut;
-    }
-
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 97 * hash + Objects.hashCode(this.bic);
-        hash = 97 * hash + Objects.hashCode(this.iban);
-        hash = 97 * hash + Objects.hashCode(this.insitut);
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.bic);
+        hash = 89 * hash + Objects.hashCode(this.iban);
         return hash;
     }
 
@@ -73,22 +54,15 @@ public class Bankverbindung implements Serializable {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        final Bankverbindung other = (Bankverbindung) obj;
+        final BankverbindungId other = (BankverbindungId) obj;
         if (!Objects.equals(this.bic, other.bic)) {
             return false;
         }
         if (!Objects.equals(this.iban, other.iban)) {
             return false;
         }
-        if (!Objects.equals(this.insitut, other.insitut)) {
-            return false;
-        }
         return true;
     }
-
-    @Override
-    public String toString() {
-        return "Bankverbindung{bic=" + bic + ", iban=" + iban + ", insitut=" + insitut + '}';
-    }
-
+    
+    
 }
