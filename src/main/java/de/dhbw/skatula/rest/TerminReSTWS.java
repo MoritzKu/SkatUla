ae6e1b43a2dd342a94a836e8778151aed4f0a55e
KanBen86 <benjamin.kanzler@gmail.com>
2019-03-25 11:25:25 +0100

    ReST-Webservice angelegt

---
+++ src/main/java/de/dhbw/skatula/rest/TerminReSTWS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dhbw.skatula.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author Benjamin Kanzler
+ */
+@Stateless
+@Path("/Termin/")
+@Consumes("application/json")
+@Produces("application/json")
+public class TerminReSTWS {
+    
+    
+    
+}
