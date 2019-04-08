3e5a8994eb4c6c81fe846fe0641cad7f6d6038f9
LiebFabian <fabianlieb@web.de>
2019-04-08 11:14:14 +0200

    Bugfixes Terminübersicht

--- src/main/java/de/dhbw/skatula/terminverwaltung/web/TerminuebersichtServlet.java
+++ src/main/java/de/dhbw/skatula/terminverwaltung/web/TerminuebersichtServlet.java
@@ -5,17 +5,22 @@
  */
 package de.dhbw.skatula.terminverwaltung.web;
 
+import de.dhbw.skatula.accounthandler.jpa.Kunde;
+import de.dhbw.skatula.accounthandler.jpa.Trainer;
 import de.dhbw.skatula.helper.Response;
+import de.dhbw.skatula.jpa.KursKunde;
 import de.dhbw.skatula.terminverwaltung.ejb.TerminBean;
 import de.dhbw.skatula.terminverwaltung.jpa.Termin;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -24,12 +29,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "TerminuebersichtServlet", urlPatterns = {"/terminuebersicht"})
 public class TerminuebersichtServlet extends HttpServlet {
 
-    
-    public static final String URL="/terminuebersicht";
-    
+    public static final String URL = "/terminuebersicht";
+
     @EJB
     protected TerminBean terminBean;
-    
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -41,60 +45,39 @@ public class TerminuebersichtServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        Response<Termin> termin = terminBean.findAll(); 
+
+        HttpSession session = request.getSession();
+        Response<Termin> termin = terminBean.findAll();
+        String nutzertyp = (String) session.getAttribute("nutzertyp");
+        if (nutzertyp.equals("kunde")) {
+            Response<Kunde> resK = new Response<>();
+            resK = (Response<Kunde>) session.getAttribute("nutzer");
+            List<Termin> terminList = new ArrayList<>();
+            for (Termin t : termin.getResponseList()) {
+                for (KursKunde kk : t.getKurs().getKursKunde()) {
+                    if (kk.getKunde().getId() == resK.getResponse().getId()) {
+                        terminList.add(t);
+                    }
+                }
+            }
+            termin.setResponseList(terminList);
+        } else if (nutzertyp.equals("trainer")) {
+            Response<Trainer> rest = new Response<>();
+            rest = (Response<Trainer>) session.getAttribute("nutzer");
+            List<Termin> terminList = new ArrayList<>();
+            for (Termin t : termin.getResponseList()) {
+                if (t.getKurs().getTrainer().getId() == rest.getResponse().getId()) {
+                    terminList.add(t);
+                }
+            }
+            termin.setResponseList(terminList);
+
+        } else {
+            termin.setResponseList(null);
+        }
         request.setAttribute("terminList", termin);
-        
+
         request.getRequestDispatcher("WEB-INF/terminuebersicht.jsp").forward(request, response);
     }
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TerminuebersichtServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TerminuebersichtServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
